ab09787732334444bde1273ece81f2d5f98e6fcf
Alla Kokhaniuk <alla.kohanjuk@gmail.com>
2023-09-02 08:14:00 +0000

    First commit.

---
+++ .ccls-cache/@home@runner@counter/header.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#ifndef _USER_DATA_H_
+#define _USER_DATA_H_
+
+
+
+class user_data {
+public:
+
+    void get_data_from_user();
+    void test();
+    void test1();
+
+protected:
+    
+    int num_a, num_b; //кількість стратегій для а і б
+    double** matr; 
+};
+
+
+
+
+#endif
+
+#pragma once
+#pragma once
